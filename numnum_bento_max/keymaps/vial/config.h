3357778e0de092537b4974eb1e24e4a1217d43cb
kiffveef <fuzuki.close+3@gmail.com>
2026-02-25 19:42:31 +0900

    ✨ Add NumNum Bento Max custom keymap
    - ベースファームウェアからの差分ファイルのみを管理
    - レイヤー数を9→6に削減
    - LEDインジケーターに3秒タイマーを追加
    - SOLID_COLORモード以外でもRGBエフェクトが動作するよう変更
    - U_H_SCROLLカスタムキーコードを削除

---
+++ numnum_bento_max/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Daraku-Neko (@Daraku-Neko)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Vial keyboard unique ID
+#define VIAL_KEYBOARD_UID {0xEA, 0xFA, 0x04, 0x88, 0x9B, 0x23, 0xD8, 0xFB}
+
+// Vial unlock combo (two keys to press to unlock)
+#define VIAL_UNLOCK_COMBO_ROWS {1, 1}
+#define VIAL_UNLOCK_COMBO_COLS {5, 6}
